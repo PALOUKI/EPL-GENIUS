2ac066b0db8263fd32f607818e982a55f080a16a
PALOUKI <paloukigodbless39@gmail.com>
2025-04-03 22:21:42 +0000

    Project Setup|Laravel 11 Inertia JS and Vue JS 3| Tailwind CSS  ZiggyVue and flowbite are installed| Theme switch (light mode/dark mode)

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { switchTheme } from "../theme";
+</script>
+
+<template>
+    <header>
+        <p>This is a simple starter kit for Laravel 11, Inertia JS, and Vue JS, without authentication. Tailwind and ZiggyVue are installed.</p>
+        <button
+            @click="switchTheme"
+            class="md:mr-5 hover:bg-slate-700 w-6 h-6 grid place-items-center rounded-full 
+            hover:outline outline-1 outline-white" 
+          >
+            <i class="fa-solid fa-circle-half-stroke"></i> 
+        </button>
+    </header>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode:'selector',
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {
+      fontFamily : {
+        Montserrat : ["Montserrat", "sans-serif"],
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ]
+}
+
